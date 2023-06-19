b765cdf580e6103bb39187509ecb23b41a882ad3
Mirosalinator <mathis.simonet2008@gmail.com>
2023-06-19 19:11:34 +0200

    lvl mathias fini
    il reste celui de louison

--- Assets/LoadLevel2.cs
+++ Assets/LoadLevel2.cs
@@ -1,18 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadLevel2 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Loadscene()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("mathias");
     }
 }
